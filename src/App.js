83cd49ab64465be80a7dcaf9d788bd3370eb0b68
Maxi Sandoval <maxisandoval98@gmail.com>
2024-01-14 14:29:36 -0300

    agregado boton inicio con icon

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from 'react';
-import { ChakraProvider, Center, Spinner, Box, Text, Flex } from '@chakra-ui/react';
+import { ChakraProvider, Center, Spinner, Box, Text, Flex, Button } from '@chakra-ui/react';
+import { AiOutlineHome } from 'react-icons/ai'; 
 import CountryInfo from './components/CountryInfo';
 import {fetchCountryData} from './api/api'
 import { useParams } from 'react-router-dom';
@@ -32,9 +33,16 @@ function App() {
     <ChakraProvider>
       <Center minHeight="100vh" backgroundColor="f5f5f5">
         <Box p={6} boxShadow="md" borderRadius="md" backgroundColor="white" maxW="lg" width="100%">
-          <Text fontSize="2xl" fontWeight="bold" mb={4}>
-              Información del pais
-          </Text>
+          <Flex justifyContent="space-between" alignItems="center" mb={4}>
+            <Text fontSize="2xl" fontWeight="bold">
+                Información del país
+            </Text>
+            {!loading && (
+              <Button colorScheme="blue" onClick={() => window.location.href='/'} leftIcon={<AiOutlineHome />}>
+                Inicio
+              </Button>
+            )}
+          </Flex>
 
           {loading && <Center mt={4}> <Spinner size="xl"/></Center>}
 
